5a925228bbaaf70fe76fdc837e70da5f3632815c
abydow <Okamiwolfie@outlook.com>
2025-10-31 10:42:08 +0530

    format printing

---
+++ formatprinting.go
@@ -0,0 +1,17 @@
+
+package main
+
+import (
+        "fmt"
+)
+
+func main() {
+        const name = "Kim"
+	const age = 22
+
+	// const name, age = "kim", 22
+
+	fmt.Printf("%s is %d years old. \n", name, age)
+	//fmt.Printf("%s is %d years old. \t and the types are %T and%T", name, age, name, age)
+	fmt.Printf("%v is %v years old. \t and the types are %T and %T ", name, age, name, age)
+}
