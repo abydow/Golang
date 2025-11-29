adc613a2390057f5252ad2387b188a4ff8d1d1c2
abydow <Okamiwolfie@outlook.com>
2025-11-29 11:41:44 +0530

    exercise 2

---
+++ exercise3/ex-02/main.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+To create a GO workspace
+
+```Bash
+// mkdir <Smaple_Go_Workspace>
+// Touch main.go
+// Go mod init <smaple.code>
+```
+
+*/
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World,👋")
+}
+
+// Go run <file_path>
