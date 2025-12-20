f55bb7574d9b785f0cf934f9f8a446b6908985d2
abydow <Okamiwolfie@outlook.com>
2025-12-21 00:07:56 +0530

    Beginning of DSA

---
+++ 12-Grouping_data_values-array_slice/01-Preview/preview.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(`
+	- Boolean
+	- Numeric
+	- String
+	- Arrays
+	- Slices
+	- Structs
+	- Pointers
+	- Functions
+	- Interfaces
+	- Maps
+	- Channels
+	`)
+
+
+}
